3a102bf25a67078dc69d9c28cfdd34080b3887f7
Tom271993 <128897129+Tom271993@users.noreply.github.com>
2023-03-25 16:47:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    anticaptchaApikey: "6e4x4776cc858485baac8a886cd0b748cf9", // paste your apikey
+    anticaptchaApikey: "e94d7a1cfb9830d7f0f999f054d882ca", // paste your apikey
     txAmount: { from: 500, to: 1000 }, // random uSei amount limits to send
     delayBetweenTransactions: 0, // milliseconds
     timeToWaitForFaucetTokens: 5, // minutes
